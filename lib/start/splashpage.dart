bd0821af11622268dc1a9cceaaea6a5ade74023c
Julia Pak <jpak0444@gmail.com>
2023-02-19 13:37:52 -0500

    ok

---
+++ lib/start/splashpage.dart
@@ -0,0 +1,29 @@
+import 'package:deputyshift/colors/colorlist.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: textturq2,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: Lottie.asset(
+                  "https://assets4.lottiefiles.com/packages/lf20_2Lm1d0.json"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
